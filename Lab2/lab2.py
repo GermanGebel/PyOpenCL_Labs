c2f19d6ce75de9b2264bad7c2b51f1e55b7dff09
Herman Gebel <germashcka@gmail.com>
2021-05-22 18:15:14 +0300

    lab2 test version

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -5,13 +5,10 @@ import sys
 from time import time
 from pprint import pprint
 
-cl.PYOPENCL_CTX = '1:2'
-
 KERNELS_FILE: str
 context: cl.Context
 queue: cl.CommandQueue
 program: None  
-tile_size = 64
 
 def initOpenCL():
     global context, queue, program
@@ -33,97 +30,147 @@ Max work groub size: {} WIs\n""".format( device.name, global_mem, local_mem, max
         properties=cl.command_queue_properties.PROFILING_ENABLE)
 
     #print("Prop: {}".format(queue.set_property(cl.command_queue_properties.PROFILING_ENABLE, True)))
-    
-    with open(KERNELS_FILE, 'r') as kernel_file:
-        program = cl.Program(context, kernel_file.read() % {'tile_size': tile_size}).build()
-        kernel_file.close()
 
-def fill_array(M, N):
-    a = []
-    for i in range(M):
-        for j in range(N):
-            a.append(i*N+j)
-    return np.array(a)
-
-def host_transpose(matr, M, N):
+def naive_host_transpose(matr, M, N):
     matr_T = np.empty_like(matr)
-    for i in range(M):
-        for j in range(N):
-            matr_T[j * M + i] = matr[i * N + j]
-    return matr_T
 
+    start = time()
 
-def lab2(M, N, check_results):
+    for row in range(M):
+        for col in range(N):
+            matr_T[col * M + row] = matr[row * N + col]
+    
+    cpu_time = time() - start
+    return matr_T, cpu_time
 
-    global context, queue, program, tile_size
+def host_transpose(matr):
+    start = time()
+    matr_T = matr.T
+    end = time()
+    cpu_time = end - start
+    return matr_T, cpu_time
 
-    does_matr_print = 0
+def global_normilize(G, L):
+    while not G % L == 0:
+        G += 1
+    return G
+
+def normilize_sizes(M, N):
+    local_size_M = 1
+    i = 0
+    barrier = 32
+    while(local_size_M <= M and local_size_M <= barrier and 2 ** (i + 1) <= barrier and M > 2 ** (i + 1)):
+        i += 1
+        local_size_M = 2 ** i
+
+    M = global_normilize(M, local_size_M)
+
+    local_size_N = 1
+    barrier = local_size_M
+    i = 0    
+    while(local_size_N <= N and local_size_N <= barrier and 2 ** (i + 1) <= barrier and N > 2 ** (i + 1)):
+        i += 1
+        local_size_N = 2 ** i
+
+    N = global_normilize(N, local_size_N)
+
+    if local_size_M > local_size_N:
+        local_size_M = local_size_N
+        M = global_normilize(M, local_size_M)
+
+    return M, N, local_size_M, local_size_N
+
+def lab2(M, N, check_results, print_results):
+    global context, queue, program, KERNELS_FILE
+
+    m, n, l_m, l_n = normilize_sizes(M, N)
+
+    global_size = (n, m)
+    local_size = (l_n, l_m)
+
+    with open(KERNELS_FILE, 'r') as kernel_file:
+        program = cl.Program(context, kernel_file.read() % {'tile_size': l_n}).build()
+        kernel_file.close()
 
     print(f"LAB 2\n[{M}x{N}] -> [{N}x{M}]\n")
     results = {}
-    kernel_names = ['global_matr_T', 'local_matr_T', '_local_matr_T']
+    kernel_names = ['global_matr_T', 'local_matr_T', 'padding_local_matr_T']
     # kernels = program.all_kernels()
 
-    matr = fill_array(M, N)
+    low = 10
+    high = 50
+
+    matr = np.random.randint(low, high, M * N).astype(np.int32)
     
-    if does_matr_print:
-        print(f"Matr: \n {[i for i in matr]}\n")
+    if print_results:
+        print("Matr: \n {}\n".format(matr.reshape((M, N))))
     #print(matr.nbytes)
     host_matr_T, device_matr_T = None, np.empty_like(matr)
     cpu_time = None
 
     if check_results:
-        start = time()
-        host_matr_T = host_transpose(matr, M, N)
-        if does_matr_print:
-            print(f"Host matr_T: \n {[i for i in host_matr_T]}")
-        cpu_time = time() - start
+        host_matr_T, cpu_time = host_transpose(matr.reshape((M, N))) #naive_host_transpose(matr, M, N)
+        if print_results:
+            print("Host matr_T: \n {}\n".format(host_matr_T))
+        
     
     buffer_in = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=matr)
     buffer_out = cl.Buffer(context, cl.mem_flags.WRITE_ONLY, matr.nbytes)
+
+    print("Global size: {}\nLocal size: {}\n".format(global_size, local_size))
     
     for i in kernel_names:
         kernel = None  
         if i == 'global_matr_T' :
             kernel = program.global_matr_T
+            # local_size = None
         elif i == 'local_matr_T':
-            kernel = program._local_matr_T
-        elif i == '_local_matr_T':
             kernel = program.local_matr_T
+            local_size = None
+        elif i == 'padding_local_matr_T':
+            kernel = program.padding_local_matr_T
+            local_size = None
         kernel.set_args(buffer_in, buffer_out, M, N)
+
         check_amount = 50
         gpu_time = 0
         for j in range(check_amount):
-            event = cl.enqueue_nd_range_kernel(queue, kernel, (M, N), None)
+            event = cl.enqueue_nd_range_kernel(queue, kernel, global_size, local_size)
             event.wait()
             gpu_time += (event.profile.end  - event.profile.start)
         gpu_time /= check_amount
+
         cl.enqueue_copy(queue, device_matr_T, buffer_out)
+        device_matr_T.shape = (N, M)
+
         data = {}
-        if does_matr_print:
-            print(f"Device matr_T: \n {[ i for i in device_matr_T]}")
+        if print_results:
+            print("Device matr_T: \n {}".format(device_matr_T))
         if check_results:            
             #print('CPU_TIME: {}s\nGPU_TIME: {}ms'.format(cpu_time, gpu_time))
-            flag = True
-            for j in range(len(host_matr_T)):
-                if host_matr_T[j] != device_matr_T[j]:
-                    flag = False
-                    print(j)
-                    break
-            data.update({"CPU_TIME (s)": cpu_time, 'Compare with host': flag})
+            comparison = host_matr_T == device_matr_T
+            is_equal = comparison.all()
+            data.update({"CPU_TIME (s)": cpu_time, 'Compare with host': is_equal})
         
-        data.update({"GPU_TIME (ms)": gpu_time/1e6})
+        data.update({"GPU_TIME (ms)": gpu_time * 1e-6})
 
-        mem_bw = (2*matr.nbytes/(gpu_time*1e-9))/pow(1024, 3)
-        efficiency = (mem_bw / 64 * 100)
+        mem_bw = (2 * device_matr_T.nbytes / pow(1024, 3)) / (gpu_time * 1e-9) # GB / s 
+        efficiency = (mem_bw * 1e-3 / 80 * 100)
         data.update({'Mem bandwidths (GB/s)': mem_bw, 'Efficiency(%)': efficiency})
 
         results.update({i: data})
     return results
 
-def graphics():
-    
-    global context, queue, program
+def new_name():
+    import os
+    files = os.listdir('bench')
+    return 'bench/test_{}.png'.format(len(files))
+
+
+def graphics():    
+    global context, queue, program, KERNELS_FILE
+    KERNELS_FILE = 'transpose_kernels.cl'
+
     platforms = cl.get_platforms()
     devices = platforms[0].get_devices(cl.device_type.GPU)
     device = devices[0]
@@ -143,20 +190,17 @@ Max work groub size: {} WIs\n""".format( device.name, global_mem, local_mem, max
 
     #print("Prop: {}".format(queue.set_property(cl.command_queue_properties.PROFILING_ENABLE, True)))
     
-    with open('kernels.cl', 'r') as kernel_file:
-        program = cl.Program(context, kernel_file.read() % {'tile_size': tile_size}).build()
-        kernel_file.close()
     sizes = [int(((2**i) // 32) * 32)
-            for i in np.arange(10, 13, 0.125)]
+            for i in np.arange(10, 13.5, 0.125)]
 
     efficiecies = {}
     
-    data = lab2(np.int32(100), np.int32(100), False)
+    data = lab2(np.int32(100), np.int32(100), False, False)
     for name in data.keys():
         efficiecies[name] = []
 
     for size in sizes:
-        data = lab2(np.int32(size), np.int32(size), False)
+        data = lab2(np.int32(size), np.int32(size), False, False)
         for name in data.keys():
             efficiecies[name].append(data[name]['Efficiency(%)'])
 
@@ -170,18 +214,23 @@ Max work groub size: {} WIs\n""".format( device.name, global_mem, local_mem, max
     legend(loc="best")
     grid()
 
-    savefig("transpose-benchmarkkk.png")
+    savefig(new_name())
     
         
 def lab2_parse_argv(argv):
     global KERNELS_FILE
+
+    check_results = False
+    print_results = False
+
     if len(argv) == 1:
         print("Enter more arguments")
         return
-    elif len(argv) == 4:        
-        check_results = False
     elif len(argv) == 5:
         check_results = True if argv[4] == 'check' else False
+    elif len(argv) == 6:
+        check_results = True if argv[4] == 'check' else False
+        print_results = True if argv[5] == 'print' else False
     else:
         print("Enter less arguments")
         return
@@ -189,12 +238,12 @@ def lab2_parse_argv(argv):
     M = np.int32(argv[2])
     N = np.int32(argv[3])
     initOpenCL()
-    pprint(lab2(M, N, check_results))
+    pprint(lab2(M, N, check_results, print_results))
 
 
 def main():   
-    lab2_parse_argv(sys.argv)
-    #graphics()
+    # lab2_parse_argv(sys.argv)
+    graphics()
 
 if __name__ == '__main__':
     main()
--- Lab2/transpose_kernels.cl
+++ Lab2/transpose_kernels.cl
@@ -1,49 +1,55 @@
-__kernel void global_matr_T(__global int* A, __global int* A_T, int M, int N) {
-    int row = get_global_id(0);
-    int col = get_global_id(1);
+__kernel void global_matr_T(
+    __global int* A, 
+    __global int* A_T, 
+    int M, int N) {
+    int col = get_global_id(0); // N
+    int row = get_global_id(1); // M
 
-    A_T[col * M + row] = A[row * N + col];
+    if (row < M && col < N)
+        A_T[col * M + row] = A[row * N + col];
     // A_T[col * N + row] = A[row * M + col];
 };
 
 #define TILE_SIZE %(tile_size)d
 
-__kernel void local_matr_T(__global int* A, __global int* A_T, int M, int N){
+__kernel void local_matr_T(
+    __global int* A, 
+    __global int* A_T, 
+    int M, int N){
     
     __local int tile[TILE_SIZE][TILE_SIZE];
 
-    int l_i = get_local_id(0);
-    int l_j = get_local_id(1);
+    int l_col = get_local_id(0);
+    int l_row = get_local_id(1);
 
-    int i = get_group_id(0) * TILE_SIZE + l_j;
-    int j = get_group_id(1) * TILE_SIZE + l_i;
+    int col = get_group_id(0) * TILE_SIZE + l_col;
+    int row = get_group_id(1) * TILE_SIZE + l_row;
 
-    if (i < M && j < N)
-        tile[l_j * TILE_SIZE][l_i] = A[j * N + i];
-    
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-    if (i < M && j < N)
-        A_T[i * M + j] = tile[l_j * TILE_SIZE][l_i];  
+    if (col < N && row < M){
+        tile[l_row][l_col] = A[row * N + col];
+        barrier(CLK_LOCAL_MEM_FENCE);
+        A_T[col * M + row] = tile[l_row][l_col];
+    }  
 }
 
-__kernel void _local_matr_T(__global int* A, __global int* A_T, int M, int N){
+__kernel void padding_local_matr_T(
+    __global int* A, 
+    __global int* A_T, 
+    int M, int N){
 
     __local int tile[TILE_SIZE][TILE_SIZE + 1];
 
-    int l_i = get_local_id(0);
-    int l_j = get_local_id(1);
+    int l_col = get_local_id(0);
+    int l_row = get_local_id(1);
 
-    int i = get_group_id(0) * TILE_SIZE + l_j;
-    int j = get_group_id(1) * TILE_SIZE + l_i;
+    int col = get_group_id(0) * TILE_SIZE + l_col;
+    int row = get_group_id(1) * TILE_SIZE + l_row;
 
-    if (i < M && j < N)
-        tile[l_j * TILE_SIZE][l_i] = A[j * N + i];
-    
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-    if (i < M && j < N)
-        A_T[i * M + j] = tile[l_j * TILE_SIZE][l_i];  
+    if (col < N && row < M){
+        tile[l_row][l_col] = A[row * N + col];
+        barrier(CLK_LOCAL_MEM_FENCE);
+        A_T[row * N + col] = tile[l_col][l_row];
+    }
 }
 
 
